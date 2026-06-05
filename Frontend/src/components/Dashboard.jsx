8a0ffc106c0ca06d904643859a461a924d7f3320
Riza Kaja <rizakaja@yahoo.com>
2026-06-05 19:07:49 +0200

    seminar te premte

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { useSelector } from 'react-redux';
+import TaskForm from './TaskForm';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const user = useSelector(state => state.user);
+  useEffect(() => {
+    if (!user) navigate("/login");
+  }, [user, navigate])
   return (
-    <div>Dashboard</div>
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Tasks Dashboard</p>
+      </section>
+
+      <TaskForm />
+    </>
   )
 }
-
 export default Dashboard
